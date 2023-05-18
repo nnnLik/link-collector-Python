58f23658c5c4edd8ce9740681221d101c1ee987c
RasulMakhmudov <makhmudov_ra@vk.com>
2023-05-18 21:40:04 +0300

    [ADD] entrypoint

---
+++ main.py
@@ -0,0 +1,40 @@
+import argparse
+import logging
+import telebot
+
+from src.configs.mongodb import mongodb
+from src.handlers.handlers import register_handlers
+
+logger = logging.getLogger(__name__)
+
+
+def main():
+    bot = telebot.TeleBot(API_TOKEN)
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    )
+
+    logger.info("Server started")
+
+    register_handlers(bot)
+
+    bot.infinity_polling()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-token", help="Telegram API token")
+    parser.add_argument("-mongo", help="MongoDB connection URI")
+    parser.add_argument("-db", help="MongoDB database name")
+
+    args = parser.parse_args()
+
+    API_TOKEN = args.token
+    MONGODB_URI = args.mongo
+    DB_NAME = args.db
+
+    mongodb.setup_connection(MONGODB_URI, DB_NAME)
+
+    main()
