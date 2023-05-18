57edb77b730293e6f3e27dd56e538eda131ef75c
RasulMakhmudov <makhmudov_ra@vk.com>
2023-05-18 21:41:24 +0300

    [ADD] const strings

---
+++ src/configs/const.py
@@ -0,0 +1,12 @@
+WELCOME_MESSAGE = "👋 Hi! Your user ID has been saved. You can now drop in links.\n\nUse /help to see a list of possible commands."
+ALREADY_REGISTERED_MESSAGE = "👤 You're already registered."
+RESET_LINKS_SUCCESS_MESSAGE = "✅ Links have been successfully reset."
+LINKS_FOUND_SUCCESS_MESSAGE = "✅ Links were successfully found."
+LINKS_NOT_FOUND_MESSAGE = "❌ The links in the message could not be recognized."
+HELP_MESSAGE = """📚 List of available commands:
+/resetlinks - reset all saved links
+/help - list of commands
+
+The LinkCollector bot will help you save random useful links and send them to you regularly throughout the day. You can add any interesting link with useful information and it will be saved. The bot will collect all added links and periodically send you multiple links in random order.
+
+Use the bot to accumulate a collection of useful links and get their inspiration and knowledge every day!"""
